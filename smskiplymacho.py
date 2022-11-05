bee4294226245e693443752d616999608eb4860f
kiplymacho <46950888+kiplymacho@users.noreply.github.com>
2022-11-05 09:21:08 +0800

    Add files via upload

---
+++ smskiplymacho.py
@@ -0,0 +1,25 @@
+import requests, os
+
+def logo():
+  print("""
+
+            | 📨 Sms-Kiplymacho 📩
+              Admin +6285751032225
+
+""")
+
+def menu():
+  os.system('clear')
+  logo()
+  print("\nMasukan Nomer Di Awali (8xxx)")
+  nomor = input("Nomer Target : 0")
+  jum = int(input("Jumlah : "))
+  for i in range(jum):
+      req = requests.get ("https://ainxbot-sms.herokuapp.com/api/Spamsms",params={"phone":nomor}).text
+      if 'terkirim' in req:
+           print("\nSpam Terkirim")
+      else:
+           print("\nSpam Gagal")
+           os.system('clear')
+
+menu()
